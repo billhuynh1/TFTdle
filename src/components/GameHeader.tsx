e8173ed0927a59c25e0b1783f666423cc0c07827
unknown <billhuynh012@gmail.com>
2024-07-13 04:07:24 -0700

    added header image, text shadow, box shadow

--- src/components/GameHeader.tsx
+++ src/components/GameHeader.tsx
@@ -2,7 +2,8 @@
 const GameHeader = () => {
     return(
         <div className="game-header">
-            <span>Guess today's TFT Champion!</span>
+            <span style={{padding: "20px", fontWeight: "bold", fontFamily: "Verdana"}}>Guess today's TFT Champion!</span>
+            <span style={{padding: "5px", paddingBottom: "25px", fontFamily: "Verdana"}}>Type any character to start.</span>
         </div>
     )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,14 @@ import GameHeader from "./GameHeader";
 
 const Header: React.FC = () => {
     return (
-        <div className="header">TFTdle</div>
-    )
-}
+        <div className="header">
+            <span style={{fontWeight: "bold", textShadow: "2px 2px 20px #000000", marginLeft: "126px", justifyContent: "center", alignItems: "center"}}>
+                TFTDLE
+            </span>
+            <img src="images/tftdle_pengu.png" className="header-image" alt="Image of penguin" />
+        </div>
+    );
+  };
+  
 
 export default Header;
