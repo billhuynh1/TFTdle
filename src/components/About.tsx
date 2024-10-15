460579fddcaff6e40a5035eb1037cf365d1bd3d3
Bill Huynh <billhuynh@MacBook-Air-82.attlocal.net>
2024-10-15 00:25:23 -0700

    updated about content

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -24,25 +24,32 @@ const About: React.FC<AboutProps> = ({ handleToggleAbout }) => {
         >
           &#x2717;
         </button>
-        <h1 style={{ fontSize: "60px" }}>About</h1>
+        <h1 style={{ fontSize: "60px", marginLeft: "50px" }}>About</h1>
         <hr />
-        <span style={{ margin: "20px", fontSize: "30px" }}>
+        <span style={{ margin: "20px", fontSize: "20px", marginLeft: "50px" }}>
           Guess today&apos; character from Teamfight Tactics!
         </span>
-        <span style={{ margin: "20px", fontSize: "30px", lineHeight: "1.5" }}>
+        <span
+          style={{
+            margin: "20px",
+            fontSize: "20px",
+            lineHeight: "1.5",
+            marginLeft: "50px",
+          }}
+        >
           TFTdle was created under Riot Games&apos; &quot;Legal Jibber
           Jabber&quot; policy using assets owned by Riot Games. Riot Games does
           not endorse or sponsor this project.
         </span>
-        <span style={{ margin: "20px", fontSize: "30px" }}>
+        <span style={{ margin: "20px", fontSize: "20px", marginLeft: "50px" }}>
           Heavily inspired by Wordle, LOLdle, Smashdle, and Pokedle.
         </span>
-        <h1 style={{ fontSize: "60px" }}>Feedback</h1>
+        <h1 style={{ fontSize: "60px", marginLeft: "50px" }}>Feedback</h1>
         <hr />
-        <span style={{ margin: "20px", fontSize: "30px" }}>
+        <span style={{ margin: "20px", fontSize: "20px", marginLeft: "50px" }}>
           Any errors? Suggestions?
         </span>
-        <span style={{ margin: "20px", fontSize: "30px" }}>
+        <span style={{ margin: "20px", fontSize: "20px", marginLeft: "50px" }}>
           DM me on discord! <u>ID: gorillagripcoochie</u>
         </span>
       </div>
