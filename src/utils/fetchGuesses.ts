2152781c6eb200c821e22e2ad9e52aab32a642cb
unknown <billhuynh012@gmail.com>
2025-02-04 01:11:14 -0800

    Developing local storage approach

--- src/utils/fetchGuesses.ts
+++ src/utils/fetchGuesses.ts
@@ -1,18 +1,25 @@
-import axios from "axios";
-import { ChampionGuess } from "../type.ts";
+// import axios from "axios";
+// import { ChampionGuess } from "../type.ts";
 
-const fetchGuesses = async (sessionId: string): Promise<ChampionGuess[]> => {
-  const API_BASE_URL = process.env.REACT_APP_BACKEND_URL;
+// const fetchGuesses = async (sessionId: string): Promise<ChampionGuess[]> => {
+//   const API_BASE_URL = process.env.REACT_APP_BACKEND_URL;
 
-  try {
-    const response = await axios.get(
-      `${API_BASE_URL}/guess/v2/${sessionId}/get`,
-    );
-    return response.data;
-  } catch (error) {
-    console.error("Error fetching guesses", error);
-    throw error;
-  }
+//   try {
+//     const response = await axios.get(
+//       `${API_BASE_URL}/guess/v2/${sessionId}/get`,
+//     );
+//     return response.data;
+//   } catch (error) {
+//     console.error("Error fetching guesses", error);
+//     throw error;
+//   }
+// };
+
+// LOCAL STORAGE DEVELOPMENT
+const fetchGuesses = (): string | null => {
+  const guesses: string | null = localStorage.getItem("guesses");
+
+  return guesses;
 };
 
 export default fetchGuesses;
