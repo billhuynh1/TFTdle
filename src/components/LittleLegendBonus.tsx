6af9f025e66b51322c2614ab52726f933cfeb133
unknown <billhuynh012@gmail.com>
2025-04-11 01:07:37 -0700

    added animations

--- src/components/LittleLegendBonus.tsx
+++ src/components/LittleLegendBonus.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { LittleLegend } from "../type.ts";
 import { useLittleLegendContext } from "../context/LittleLegendContext.tsx";
 
@@ -10,6 +10,22 @@ const LittleLegendBonus: React.FC<LittleLegendBonusProps> = ({
   littleLegend,
 }) => {
   const { littleLegendList, littleLegendAnswer } = useLittleLegendContext();
+  const [bonusAnswer, setBonusAnswer] = React.useState<string | undefined>("");
+  const [choice, setChoice] = React.useState<string>("");
+
+  useEffect(() => {
+    if (littleLegendAnswer) {
+      const variant: string | undefined = littleLegendAnswer.name.replaceAll(
+        `${littleLegendAnswer.baseType}_`,
+        "",
+      );
+      setBonusAnswer(variant);
+    }
+  }, [littleLegendAnswer]);
+
+  // const handleClick = () => {
+  //   setChoice()
+  // };
 
   // Extracts the variants from the base type of the little legend
   const littleLegendVariants: string[] = littleLegendList
@@ -28,8 +44,15 @@ const LittleLegendBonus: React.FC<LittleLegendBonusProps> = ({
       </span>
       <div className="little-legend-bonus__content">
         {littleLegendVariants.map((variant) => (
-          <button type="button" className="little-legend-bonus__btn">
-            {variant.replaceAll("_", " ")}
+          <button
+            type="button"
+            className="little-legend-bonus__btn"
+            onClick={() => setChoice(variant)}
+            key={variant}
+          >
+            {variant
+              .replaceAll("_", " ")
+              .replace(littleLegend?.baseType ?? "", "Default")}
           </button>
         ))}
       </div>
