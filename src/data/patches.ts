2a75fa89aa61d5c4155c5796cf190cefd18a879a
unknown <billhuynh012@gmail.com>
2025-05-10 20:25:57 -0700

    Deployment

---
+++ src/data/patches.ts
@@ -0,0 +1,38 @@
+import { PatchNote } from "../type.ts";
+
+const patches: PatchNote[] = [
+  {
+    version: "v1.4",
+    date: "5/05/2025",
+    changes: [
+      "Included instructions for every game mode. Click the question mark at the top to view.",
+      "Updated contact info. Included Reddit and Tiktok",
+    ],
+  },
+  {
+    version: "v1.3",
+    date: "5/04/2025",
+    changes: [
+      "Added new 'daily streak' tracker",
+      "Fixed a bug with finisher guesses not saving",
+    ],
+  },
+  {
+    version: "v1.2",
+    date: "5/01/2025",
+    changes: [
+      "Updated navbar UI",
+      "Updated game ending screen UI",
+      "Changed button icon",
+    ],
+  },
+  {
+    version: "v1.0",
+    date: "4/30/2025",
+    changes: [
+      "Trait game mode added, guess the trait based on the description!",
+    ],
+  },
+];
+
+export default patches;
